df9330484e565cdfa14d56d7c28dda0cf2e83788
alaalsalam <alaalsalam101@gmail.com>
2024-05-14 09:55:02 +0200

    remove daily_cash_flow_transactions and add fild to service_invoice

--- services_company/services_company/doctype/daily_cash_flow_transactions/daily_cash_flow_transactions.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2024, alaalsalam and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class DailyCashFlowTransactions(Document):
-	pass
